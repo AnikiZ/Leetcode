270699f07fc45a083303c30fc888f0d2ce3e3fe7
AnikiZ <zepingz@andrew.cmu.edu>
2022-08-29 17:00:27 -0400

    20220828

--- Code/Java/Tree/RecoverBinarySearchTree_99.java
+++ Code/Java/Tree/RecoverBinarySearchTree_99.java
@@ -2,21 +2,58 @@
  * @Author: Zeping Zhu
  * @Andrew ID: zepingz
  * @Date: 2022-08-24 01:59:08
- * @LastEditTime: 2022-08-24 02:01:10
+ * @LastEditTime: 2022-08-25 19:43:28
  * @LastEditors: Zeping Zhu
  * @Description: 
- * @FilePath: /Code/Java/Tree/RecoverBinarySearchTree_99.java
+ * @FilePath: /Java/Tree/RecoverBinarySearchTree_99.java
  */
 package Tree;
 
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class RecoverBinarySearchTree_99 {
-    class Solution {
-        public void recoverTree(TreeNode root) {
-            Deque<TreeNode> stack = new LinkedList<>();
-            TreeNode x = null, y = null, prevSmaller = null;
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
+    }
+    public void recoverTree(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode x = null, y = null, prevSmaller = null;
+        // if (root != null) {
+        //     stack.push(root);
+        // }
+        // 加一个或者root != null不然初始过不了
+        while (!stack.isEmpty() || root != null) {
+            // 还是从root判断，否则
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prevSmaller != null && root.val < prevSmaller.val) {
+                y = root;
+                if (x == null) {
+                    x = prevSmaller;
+                } else {
+                    break;
+                }
+            }
+            prevSmaller = root;
+            // 有可能为null!，所以上面while还是以root判断比较好
+            root = root.right;
+        }
+        // swap
+        int tmp = x.val;
+        x.val = y.val;
+        y.val = tmp;
     }
 }
--- Code/Java/Tree/TrimABinarySearchTree_669.java
+++ Code/Java/Tree/TrimABinarySearchTree_669.java
@@ -2,13 +2,40 @@
  * @Author: Zeping Zhu
  * @Andrew ID: zepingz
  * @Date: 2022-08-24 15:08:12
- * @LastEditTime: 2022-08-24 15:08:12
+ * @LastEditTime: 2022-08-25 19:45:33
  * @LastEditors: Zeping Zhu
  * @Description: 
- * @FilePath: /Code/Java/Tree/TrimABinarySearchTree_669.java
+ * @FilePath: /Java/Tree/TrimABinarySearchTree_669.java
  */
 package Tree;
 
 public class TrimABinarySearchTree_669 {
-    
+    class Solution {
+        public static class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public TreeNode trimBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val > high) {
+                return trimBST(root.left, low, high);
+            }
+            if (root.val < low) {
+                return trimBST(root.right, low, high);
+            }
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
 }
