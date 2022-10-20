e8dde2dba22e2865169648ff94ebaf170c1c1aff
AnikiZ <zepingz@andrew.cmu.edu>
2022-10-20 02:28:25 -0400

    feature: lc graph

--- Code/Java/Graph/AllPathsFromSourceLeadToDestination_1059.java
+++ Code/Java/Graph/AllPathsFromSourceLeadToDestination_1059.java
@@ -6,7 +6,7 @@ import java.util.List;
  * @Author: Zeping Zhu
  * @Andrew ID: zepingz
  * @Date: 2022-10-19 20:18:31
- * @LastEditTime: 2022-10-19 20:57:59
+ * @LastEditTime: 2022-10-19 21:20:42
  * @LastEditors: Zeping Zhu
  * @Description: package Graph;
  * @FilePath: /Leetcode/Code/Java/Graph/AllPathsFromSourceLeadToDestination_1059.java
@@ -21,6 +21,10 @@ class Solution {
         for (int[] edge : edges) {
             roads.get(edge[0]).add(edge[1]);
         }
+        // corner case: edges:[], src==dest
+        if (roads.get(source).size() == 0 && source == destination) {
+            return true;
+        }
         boolean res = false;
         int[] visited;
         for (int node : roads.get(source)) {
@@ -29,7 +33,7 @@ class Solution {
             res = dfs(node, destination, roads, visited);
             if (res == false) {
                 return false;
-            } 
+            }
         }
         return res;
     }
@@ -37,16 +41,21 @@ class Solution {
         if (visited[node] == 1) {
             return false;
         } else {
-            visited[node] = 1;
-            if (node == destination && roads.get(node).size()==0) {
-                return true;
+            if (roads.get(node).size() == 0) {
+                if (node == destination) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
+            visited[node] = 1;
             boolean res = false;
             for (int n : roads.get(node)) {
                 res = dfs(n, destination, roads, visited);
                 if (res == false) {
                     return false;
                 }
+                visited[node] = 0;
             }
             return res;
         }
